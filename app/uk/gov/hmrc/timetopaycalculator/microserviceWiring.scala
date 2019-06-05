6a6dcbbd4661e3794c3d10a528740ada6504ecbe
Andrew Richards <42971995+andrewrichards1@users.noreply.github.com>
2019-06-05 08:37:10 +0100

    [AJER][OPS-2598] Fixing build after play upgrade (#28)

--- app/uk/gov/hmrc/timetopaycalculator/microserviceWiring.scala
+++ app/uk/gov/hmrc/timetopaycalculator/microserviceWiring.scala
@@ -16,24 +16,28 @@
 
 package uk.gov.hmrc.timetopaycalculator
 
+import akka.actor.ActorSystem
 import com.typesafe.config.Config
-import uk.gov.hmrc.http._
+import play.api.{Configuration, Play}
 import uk.gov.hmrc.http.hooks.HttpHooks
+import uk.gov.hmrc.http.{HttpDelete, HttpGet, HttpPost, HttpPut, _}
 import uk.gov.hmrc.play.audit.http.HttpAuditing
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
-import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
+import uk.gov.hmrc.play.config.{AppName, ServicesConfig}
 import uk.gov.hmrc.play.http.ws._
 import uk.gov.hmrc.play.microservice.config.LoadAuditingConfig
-import uk.gov.hmrc.timetopaycalculator.Config.{DefaultAppName, DefaultRunMode}
+import uk.gov.hmrc.timetopaycalculator.Config.DefaultRunMode
 
-trait Hooks extends HttpHooks with HttpAuditing{
+trait Hooks extends HttpHooks with HttpAuditing {
   override val hooks = Seq(AuditingHook)
   override lazy val auditConnector: AuditConnector = MicroserviceAuditConnector
 }
 
-trait WSHttp extends HttpGet with WSGet with HttpPut with WSPut with HttpPost with WSPost with HttpDelete with WSDelete with HttpPatch with WSPatch with Hooks with DefaultAppName{
-  override lazy val configuration: Option[Config] = None
+trait WSHttp extends HttpGet with WSGet with HttpPut with WSPut with HttpPost with WSPost with HttpDelete with WSDelete with Hooks with WSPatch with HttpPatch with AppName {
+  override protected def appNameConfiguration: Configuration = Play.current.configuration
+  override protected def actorSystem: ActorSystem = Play.current.actorSystem
+  override protected def configuration: Option[Config] = Option(Play.current.configuration.underlying)
 }
 
 object WSHttp extends WSHttp
